73ebe375c70ee335feb27ef3cc8448802c179ead
novakovicb1 <novakovicb489@gmail.com>
2023-11-15 15:51:45 +0100

    Examples with operators
    Unary
    Aritmetic
    Compariosn
    Operators on the bit level
    Intro  to conrol flow

---
+++ src/main/java/ba/smoki/two/operator/BitDemo.java
@@ -0,0 +1,9 @@
+package ba.smoki.two.operator;
+
+public class BitDemo {
+    public static void main(String[] args) {
+        int number=24;
+        int resul=number>>2;
+        System.out.println(resul);
+    }
+}
